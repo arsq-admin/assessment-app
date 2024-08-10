4c7b8053045a8b647dd341b4159e1011b37bdc76
Alex Wong <wonghoming.home@gmail.com>
2024-08-10 22:50:59 +0100

    auth0 login added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { AssessmentPage } from "./assessmentPage";
 import styled from "styled-components";
 import { Container, FluidContainer } from "./components";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = styled(FluidContainer)`
   border-bottom: 8px solid #ebebeb;
@@ -8,14 +9,25 @@ const Header = styled(FluidContainer)`
   align-items: center;
 `;
 
+function LoginButton() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return !isAuthenticated ? (
+    <button onClick={() => loginWithRedirect()}>Log in</button>
+  ) : (
+    <button onClick={() => logout()}>log out</button>
+  );
+}
+
 function App() {
   return (
-    <div>
+    <>
       <Header>
         <Container>Tender name</Container>
+        <LoginButton />
       </Header>
       <AssessmentPage />
-    </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./main.css";
 import "@scottish-government/design-system/dist/css/design-system.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-z81t01f1zfr00fgx.us.auth0.com"
+      clientId="7nOSTka7pBbrY77drVlce5BSwTeqmsIY"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>
 );
