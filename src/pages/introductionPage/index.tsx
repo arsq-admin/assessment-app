b953c00cbcfc06d0b2edf537885504b691307e9a
tyrellyu <tyrellyu@gmail.com>
2024-09-24 16:48:47 +0100

    Added assessment name to intro page

--- src/pages/introductionPage/index.tsx
+++ src/pages/introductionPage/index.tsx
@@ -1,7 +1,15 @@
-export const IntroductionPage = () => {
-  return (
+import { useContext } from 'react';
+import { AssessmentContext } from "@/context";
 
+export const IntroductionPage = () => {
+  const { config} = useContext(AssessmentContext);
+  return (
+    <div>
     <div>Hello World</div>
+    <h1>{config?.name}</h1>
+
+
+    </div>
     
   )
 }
