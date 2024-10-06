d4030d58beea2ec03c507dbe2bd4c1a939c68d4b
Alex Wong <wonghoming.home@gmail.com>
2024-10-06 20:24:17 +0100

    basic styling for the successful and unsuccessful page

--- src/pages/resultPage/components/ImprovementPlanSection.tsx
+++ src/pages/resultPage/components/ImprovementPlanSection.tsx
@@ -1,11 +1,16 @@
 export const ImprovementPlanSection = () => {
   return (
-    <div>
-      <p>
+    <div className="ds_inset-text">
+      <p className="ds_inset-text__text">
         However, you can complete an Improvement plan to detail the changes you
         will make to meet the requirements of the tender.
       </p>
-      <p>You are required to submit an improvement plan for 3 questions.</p>
+      <p className="ds_inset-text__text">
+        You are required to submit an improvement plan for 3 questions.
+      </p>
+      <button className="ds_button" type="button">
+        Start your improvement plan
+      </button>
     </div>
   );
 };
--- src/pages/resultPage/components/SuccessfulPage.tsx
+++ src/pages/resultPage/components/SuccessfulPage.tsx
@@ -6,8 +6,21 @@ interface Props {
 export const SuccessfulPage = ({ title = "", body = "" }: Props) => {
   return (
     <>
-      <p>Thank you for your submission</p>
-      <h1>{title}</h1>
+      <div className="ds_notification-panel">
+        <h1 className="ds_notification-panel__title">
+          Thank you for your submission
+        </h1>
+        <p className="ds_notification-panel__content">
+          You have met the requirements for this assessment.
+        </p>
+        <p className="ds_notification-panel__content">
+          We’ve sent you a confirmation email.
+        </p>
+        <p className="ds_notification-panel__content">
+          Check your spam inbox if you do not get an email.
+        </p>
+      </div>
+      <h2>{title}</h2>
       <p>{body}</p>
       <h2>What happens next?</h2>
       <p>
