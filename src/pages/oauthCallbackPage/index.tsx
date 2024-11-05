af0ebf5615db5d777892d96ee6995fe5e9992c97
Alex Wong <wonghoming.home@gmail.com>
2024-11-05 10:16:47 +0000

    the callback page, now calls the login api from user service to translate the code into an access token in the http cookies

--- src/pages/oauthCallbackPage/index.tsx
+++ src/pages/oauthCallbackPage/index.tsx
@@ -1,11 +1,18 @@
-import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 export const OauthCallbackPage = () => {
   const [searchParams] = useSearchParams();
-  useEffect(() => {
-    const code = searchParams.get("code");
-  }, [searchParams]);
+  const code = searchParams.get("code");
+  const { data } = useQuery({
+    queryKey: [code],
+    queryFn: () => {
+      return fetch(
+        `https://user-wong.supply25.com/login?code=${code}&redirect=http://localhost:5173/oauth/callback`
+      );
+    },
+  });
 
+  console.log("data", data);
   return <div>oauth callback</div>;
 };
