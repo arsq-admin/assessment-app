11037893ff9c061328148682d3bf674814203989
Alex Wong <wonghoming.home@gmail.com>
2024-12-11 09:58:09 +0000

    added export button to the improvement plan result page

--- src/pages/improvementPlanResultPage/index.tsx
+++ src/pages/improvementPlanResultPage/index.tsx
@@ -1,8 +1,21 @@
+import { exportResultAsPdf } from "@/api/assessment";
 import { Column, Container, PoweredBySupply25 } from "@/components";
+import { TenderPackageContext, UserContext } from "@/context";
 import { scotGovColour } from "@/themes";
+import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
 
 export const ImprovementPlanResultPage = () => {
   const { positive, white, secondaryText } = scotGovColour;
+  const { organisations } = useContext(UserContext);
+  const { tenderPackage } = useContext(TenderPackageContext);
+  const { urlId = "" } = useParams();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: exportResultAsPdf,
+  });
+
   return (
     <Container padding="4rem 0 10rem">
       <Column span="2 / span 10" margin="0 0 4rem">
@@ -17,6 +30,23 @@ export const ImprovementPlanResultPage = () => {
             <p style={{ margin: "0" }}>We’ve sent you a confirmation email.</p>
             <p>Check your spam inbox if you do not get an email.</p>
           </div>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <button
+            style={{ margin: "1.5rem 0" }}
+            className="ds_button"
+            disabled={isPending}
+            onClick={() =>
+              mutate({
+                organisationId: organisations[0]?.id,
+                urlId,
+                tenderName: tenderPackage?.name || "",
+                pcsId: tenderPackage?.pcsId || "",
+              })
+            }
+          >
+            Download PDF Result
+          </button>
         </div>
       </Column>
       <Column span="3 / span 8">
